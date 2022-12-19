52c109aba7557ce69037b85103165044db2e5df4
Bitten <e_hlers@live.dk>
2022-12-19 02:42:58 +0100

    Finish

---
+++ 5_Assemblies/Program.cs
@@ -0,0 +1,24 @@
+﻿using System.Reflection;
+
+public class Test
+{
+    [Obsolete("method1 is obsolete", true)]
+    public static int TestAttr()
+    {
+        return 1;
+    }
+
+    static void Main(string[] args)
+    {
+
+        long longtest = 1;
+        
+        int inttest = 1; // = TestAttr();
+
+        Console.WriteLine($"Type of long: {longtest.GetType()}, type of int {inttest.GetType()}");
+
+
+        Assembly info = typeof(string).Assembly;
+        Console.WriteLine(info);
+    }
+}
--- 6_Thread/Program.cs
+++ 6_Thread/Program.cs
@@ -1,5 +1,8 @@
 ﻿
 
+using System.Collections.Concurrent;
+using System.Globalization;
+
 class ThreadCreationProgram
 {
     public static void CallToChildThread()
@@ -7,7 +10,7 @@ class ThreadCreationProgram
         Console.WriteLine("Child thread starts");
 
         // the thread is paused for 5000 milliseconds
-        int sleepfor = 5000;
+        int sleepfor = 1000;
 
         Console.WriteLine("Child Thread Paused for {0} seconds", sleepfor / 1000);
         Thread.Sleep(sleepfor);
@@ -22,5 +25,68 @@ class ThreadCreationProgram
         Thread childThread = new Thread(childref);
         childThread.Start();
         Console.ReadKey();
+
+
+        // Parallel
+        var numbers = new List<int>();
+        for(int i = 0; i < 100; i++)
+        {
+            numbers.Add(i);
+        }
+
+        foreach(var n in numbers)
+        {
+            if(IsPrime(n))
+            {
+                Console.WriteLine(n);
+            }
+        }
+
+        Console.WriteLine("-------------------------------");
+
+        Parallel.ForEach(numbers, n =>
+        {
+            if (IsPrime(n))
+            {
+                Console.WriteLine(n);
+            }
+        });
+    }
+
+    private static IList<int> GetPrimeListWithParallel(IList<int> numbers)
+    {
+        var primeNumbers = new ConcurrentBag<int>();
+
+        Parallel.ForEach(numbers, number =>
+        {
+            if (IsPrime(number))
+            {
+                primeNumbers.Add(number);
+            }
+        });
+
+        return primeNumbers.ToList();
+    }
+
+    /// <summary>
+    /// IsPrime returns true if number is Prime, else false.(https://en.wikipedia.org/wiki/Prime_number)
+    /// </summary>
+    /// <param name="number"></param>
+    /// <returns></returns>
+    private static bool IsPrime(int number)
+    {
+        if (number < 2)
+        {
+            return false;
+        }
+
+        for (var divisor = 2; divisor <= Math.Sqrt(number); divisor++)
+        {
+            if (number % divisor == 0)
+            {
+                return false;
+            }
+        }
+        return true;
     }
 }
